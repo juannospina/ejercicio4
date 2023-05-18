02cdc394805aca20c0fa7e2e797201852bb86cfd
Juan Ospina <jeol3739@gmail.com>
2023-05-18 14:52:44 -0500

    Ejercicio 4 Java Intro

---
+++ src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,16 @@
+
+package ejercicio4;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        
+        nombre = "Juan Esteban";
+        edad = 26;
+        
+        System.out.println("Nombre: " + nombre + ", edad: " + edad + "años.");
+    }
+    
+}
